83d3b181c8f720bf4073b06381117cdbd51ae472
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-08 19:50:03 -0800

    Test 1 passed.

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -8,6 +8,10 @@ Write a function named sortBackwards that takes in an array of numbers and retur
 
 const sortBackwards = arr => {
   // Solution code here...
+  arr.sort((a, b) => {
+    return b - a;
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
