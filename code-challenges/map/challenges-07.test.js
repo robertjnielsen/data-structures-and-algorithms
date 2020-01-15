d7907aabd11efa1d6833739eae63e88d1ac2e702
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-14 22:44:33 -0800

    Test 3 Passed

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -27,6 +27,9 @@ Write a function named forEachTwoToThe that produces the same output as your for
 
 const forEachTwoToThe = arr => {
   // Solution code here...
+  let newArr = [];
+  arr.forEach(num => newArr.push(Math.pow(2, num)));
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -37,6 +40,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = arr => {
   // Solution code here...
+  return arr.map(num => Math.pow(2, num));
 };
 
 /* ------------------------------------------------------------------------------------------------
